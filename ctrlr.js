cc431e9a8f0315dc44f660adff723aacb6e794b7
the loyaltouch <loyaltouch@gmail.com>
2017-08-21 11:01:04 +0900

    アイテム装備機能の追加

--- ctrlr.js
+++ ctrlr.js
@@ -10,8 +10,26 @@ function do_select(scene){
   });
 }
 
+function do_equip(id){
+  let item_name = $("#items_1_" + id + "_name").text();
+  if(item_name){
+    $("#equip_name").text(item_name);
+    window._g.members.you.equip = window._g.safe_get_item(item_name);
+    reflesh();
+  }
+}
+
 function reflesh(){
   let g = window._g;
+  // ステータス欄の再描画
+  reflesh_status(g.members.you);
+
+  // アイテム欄の再描画
+  reflesh_items(1);
+  reflesh_items(2);
+  reflesh_items(0);
+  
+  // 本文の再描画
   let tagged = g.message.replace(/\n/g, "<br />");
   $("#message").html(tagged);
 
@@ -23,6 +41,28 @@ function reflesh(){
   if(g.select){
     for(let i = 0; i < g.select.length; i++){
       $("#select").append(build_li(g.select[i].label, g.select[i].link));
+    }
+  }
+}
+
+function reflesh_status(member){
+  $("#" + member.id + "_vit_max").text(member.vit_max);
+  $("#" + member.id + "_vit_now").text(member.vit_now);
+  $("#" + member.id + "_lck_max").text(member.lck_max);
+  $("#" + member.id + "_lck_now").text(member.lck_now);
+  $("#" + member.id + "_dex_max").text(member.get_dex());
+}
+
+function reflesh_items(type){
+  let items = window._g.items;
+  let rows = 1;
+  for(let name in items){
+    if(items[name].type == type){
+      if(items[name].count > 0){
+        $("#items_" + type + "_" + rows + "_name").text(name);
+        $("#items_" + type + "_" + rows + "_count").text(items[name].count);
+        rows++;
+      }
     }
   }
 }
--- logic/logic.js
+++ logic/logic.js
@@ -1,17 +1,25 @@
 class Chara{
-  constructor(name, vit, dex, lck){
+  constructor(id, name, vit, dex, lck){
+    this.id = id;
     this.name = name;
-    this.vit = vit;
+    this.vit_max = this.vit_now = vit;
     this.dex = dex;
-    this.lck = lck;
+    this.lck_max = this.lck_now = lck;
+    this.equip = { type: 1, value: 0, prise: 0 };
+  }
+
+  get_dex(){
+    return this.dex + this.equip.value;
   }
 }
 
 class Game{
   constructor(){
+    this.items = get_item_data();
     this.members = {};
-    var you = new Chara("あなた", 12, 7, 9);
-    this.members.あなた = you;
+    var you = new Chara("you", "あなた", 12, 7, 9);
+    you.equip = this.items.ナイフ;
+    this.members.you = you;
     this.next = "start";
     this.message = "";
   }
@@ -31,6 +39,14 @@ class Game{
         let entry = {label: data.select[i], link: data.select[i + 1]};
         this.select.push(entry);
       }
+    }
+  }
+
+  safe_get_item(item_name){
+    if(this.items[item_name]){
+      return this.items[item_name];
+    }else{
+      return this.items[0];
     }
   }
 }
